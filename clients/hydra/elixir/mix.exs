751e144458b6bc2dec7f38ff173a1b0858ecfc77
Paul Balomiri <paulbalomiri@gmail.com>
2022-11-19 17:27:18 +0200

    minor bugfix

--- clients/hydra/elixir/mix.exs
+++ clients/hydra/elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Ory.Hydra.Mixfile do
   def project do
     [
       app: :ory_hydra,
-      version: "v2.0.2",
+      version: "2.0.2",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
